5dcce38343d4cf4409eca621d510e03df375e263
xieliming2017 <xieliming2006@hotmail.com>
2017-11-06 09:36:53 -0500

    Assignment 317 crud: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      post.title = "SPAM" if  index % 5 == 0
+    end
   end
 
   def show
