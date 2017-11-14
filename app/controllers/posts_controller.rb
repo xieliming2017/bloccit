4ecc59197d22c8773413a4b57b075078fa3a3315
xieliming2017 <xieliming2006@hotmail.com>
2017-11-14 11:28:51 -0500

    Checkpoint 318 Rails CRUD: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,31 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts.each_with_index do |post, index|
-      post.title = "SPAM" if  index % 5 == 0
-    end
+    # @posts.each_with_index do |post, index|
+    #   # post.title = "SPAM" if  index % 5 == 0
+    # end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
