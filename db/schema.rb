b901e79937c1ade8c292413f00d51829fee59cc4
xieliming2017 <xieliming2006@hotmail.com>
2017-11-15 14:00:21 -0500

    Assignment-319-more-crud: Completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031185356) do
+ActiveRecord::Schema.define(version: 20171115170736) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -31,6 +39,14 @@ ActiveRecord::Schema.define(version: 20171031185356) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe Question, type: :model do
   let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: false)}
 
   describe "attributes" do
-    it "has title, body and resloved attributes" do
+    it "has title, body and resolved attributes" do
       expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved: false)
     end
+  end
+
+  it "should respond to title" do
+    expect(question).to respond_to(:title)
+  end
+
+  it "should respond to body" do
+    expect(question).to respond_to(:body)
+  end
+
+  it "should respond to resolved" do
+    expect(question).to respond_to(:resolved)
   end
 end
