6d255db4fb5284e87980d782a9f23874ab85702c
xieliming2017 <xieliming2006@hotmail.com>
2017-11-23 16:08:18 -0500

    Checkpoint 322 validation: Completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,6 +10,12 @@ let(:topic) {Topic.create!(name: name, description: description)}
 let(:post) {topic.posts.create!(title: title, body: body)}
 
 it {is_expected.to belong_to(:topic)}
+it {is_expected.to validate_presence_of(:title)}
+it {is_expected.to validate_presence_of(:body)}
+it {is_expected.to validate_presence_of(:topic)}
+
+it {is_expected.to validate_length_of(:title).is_at_least(5) }
+it {is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "has title and body attributes" do
