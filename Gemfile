a4555d5dc49285fb5a136bdc68dff73e1314984d
xieliming2017 <xieliming2006@hotmail.com>
2017-11-24 01:29:33 -0500

    Checkpoint 323 authentication: Completed

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ group :development, :test do
 end
 
 gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117040839) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string "title"
-    t.text "copy"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20171124013425) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -57,6 +49,14 @@ ActiveRecord::Schema.define(version: 20171117040839) do
     t.string "name"
     t.boolean "public", default: true
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
