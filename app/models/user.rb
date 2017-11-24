e5c76644bd8ccd2d1312c8b8336857b5a094aff8
xieliming2017 <xieliming2006@hotmail.com>
2017-11-24 02:23:12 -0500

    Assignment 323 authentication: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.name = name.split(" ").map{|word| word.capitalize}.join(" ") if name.present?}
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format the user's name" do
+      user.name = "bloccit user"
+      user.save
+      expect(user.name).to eq "Bloccit User"
+    end
   end
 
   describe "invalid user" do
@@ -32,6 +38,7 @@ RSpec.describe User, type: :model do
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
     end
-
   end
+
+
 end
