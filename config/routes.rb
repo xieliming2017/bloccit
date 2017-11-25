f5efd2094b1483584e4c612ddc833a7834a44640
xieliming2017 <xieliming2006@hotmail.com>
2017-11-25 16:38:18 -0500

    assignment 324: first commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
