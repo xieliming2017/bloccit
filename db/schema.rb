dc857040c7a5ae9e1e80ba930429fc275b18c07e
xieliming2017 <xieliming2006@hotmail.com>
2017-12-02 01:56:39 -0500

    Checkpoint-329-voting: Completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201054515) do
+ActiveRecord::Schema.define(version: 20171202062726) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171201054515) do
     t.datetime "updated_at", null: false
     t.integer "topic_id"
     t.integer "user_id"
+    t.float "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -64,6 +65,16 @@ ActiveRecord::Schema.define(version: 20171201054515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
