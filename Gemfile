6e96286533fb811177834bc26252d54bc4b772c8
xieliming2017 <xieliming2006@hotmail.com>
2017-12-03 01:44:39 -0500

    Checkpoint-330-favorites: Completed

--- Gemfile
+++ Gemfile
@@ -48,3 +48,5 @@ end
 gem 'bootstrap-sass'
 
 gem 'bcrypt'
+
+gem 'figaro', '1.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   default_scope { order('rank DESC') }
 
